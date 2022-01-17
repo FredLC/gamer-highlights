1cb6d75197449d83f7cb594950be627270262200
Fred <fredlefevre@outlook.com>
2022-01-17 17:28:01 -0500

    Created alert component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  purge: {
+    content: ["./src/**/*.{html,ts}"],
+    options: {
+      safelist: ["bg-blue-400", "bg-green-400", "bg-red-400"],
+    },
+  },
   theme: {
     extend: {},
   },
